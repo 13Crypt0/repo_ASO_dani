d9e268c5c70cb683410015b410e2f7dd2ffa817f
Dani989898 <danmarcol2@alu.edu.gva.es>
2024-09-24 10:40:21 +0200

    20 acabar

--- Unidad-1-bash/ejercicio20.sh
+++ Unidad-1-bash/ejercicio20.sh
@@ -9,3 +9,18 @@ elif [[ $1 -le 0 ]]; then
   echo "Numero negativo. Introduce numeros positivos"
   exit  1
 fi
+
+for ((i=1; i<=$1; i++)); do
+  calculo=$(( $1 % i ))
+  echo "$calculo" | grep ^0 >> texto.txt
+done
+
+resultado=cat texto.txt | bc -l
+
+if [[ $resultado -eq 2 ]]; then
+  echo "No es un numero primo"
+else
+  echo "Es un numero primo"
+fi
+
+rm texto.txt
