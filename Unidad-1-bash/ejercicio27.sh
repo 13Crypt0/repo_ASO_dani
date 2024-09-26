337d1753f4793e96d03412057f56964217a99d67
Dani <danielmartinezcolomerr@gmail.com>
2024-09-26 18:06:32 +0200

    Acabar ultimo

--- Unidad-1-bash/ejercicio27.sh
+++ Unidad-1-bash/ejercicio27.sh
@@ -16,4 +16,4 @@ function existe() {
   fi
 }
 
-revisar "$fichero"
+existe "$fichero"
---
+++ Unidad-1-bash/ejercicio28.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+#Falta nombre de la red
+function informacion() {
+  ip=$(ip -o -f inet addr show enp0s3 | awk '{print $4}' | awk -F '/' '{print $1}')
+  CIDR=$(ip r | grep src | cut -d" " -f 1)
+  broadcast=$(ip -o -f inet addr show enp0s3 | awk '{print $6}')
+  marcara=$(ip -o -f inet addr show enp0s3 | awk '{print $4}' | awk -F '/' '{print $2}') #FALLO NO DEVUELVE MASCARA AL HABER DOS FILTRADOS
+
+echo "IP del equipo: $ip"
+echo "CIDR: $CIDR"
+echo "Broadcast: $broadcast"
+echo "Máscara de subred: /$mascara"
+}
+
+informacion
