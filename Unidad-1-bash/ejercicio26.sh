5c4978bf9d38ad40d6684bd7dcb48cadb0993d0e
Dani989898 <danmarcol2@alu.edu.gva.es>
2024-09-26 13:02:29 +0200

    Acabar 2 ultimos

--- Unidad-1-bash/ejercicio26.sh
+++ Unidad-1-bash/ejercicio26.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#acabar, pilla todos a la vez el find por eso no ejecuta
 clear
 
 echo "Generando lista archivos peligosos..."
 
-#Enlaces simbolicos eliminados
-
 resultado=$(find / -exec ls -l {} \; 2> /dev/null | egrep '(^[-drwxst]{7}[rwx]{3})')
 
 for i in $resultado; do
-  echo "$i" | egrep "^.*/.*$"
+  echo "$i" | egrep "^.*/.*$" >> archivos_peligrosos.txt
   ((contador++))
 done
---
+++ Unidad-1-bash/ejercicio27.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+
+fichero=$1
+
+function existe() {
+  if [[ $# -ne 1 ]]; then
+    echo "Introduce 1 parametro"
+    exit 1
+  elif [[ ! -f $fichero ]]; then
+    echo "El fichero no se encuentra. Introduce la ruta absoluta de este"
+    exit 1
+  else
+    chmod u+x $fichero
+  fi
+}
+
+revisar "$fichero"
