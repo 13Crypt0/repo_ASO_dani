943d824a1bf8dea16f2d4967a40759e86f4689b2
Dani989898 <danmarcol2@alu.edu.gva.es>
2024-09-27 09:58:18 +0200

    enp cambiar

--- Unidad-1-bash/ejercicio28.sh
+++ Unidad-1-bash/ejercicio28.sh
@@ -6,7 +6,7 @@ function informacion() {
   ip=$(ip -o -f inet addr show enp0s3 | awk '{print $4}' | awk -F '/' '{print $1}')
   CIDR=$(ip r | grep src | cut -d" " -f 1)
   broadcast=$(ip -o -f inet addr show enp0s3 | awk '{print $6}')
-  marcara=$(ip -o -f inet addr show enp0s3 | awk '{print $4}' | awk -F '/' '{print $2}') #FALLO NO DEVUELVE MASCARA AL HABER DOS FILTRADOS
+  marcara=$(ip -o -f inet addr show enp9s0 | awk '{print $4}' | awk -F '/' '{print $2}') #FALLO NO DEVUELVE MASCARA AL HABER DOS FILTRADOS
 
 echo "IP del equipo: $ip"
 echo "CIDR: $CIDR"
