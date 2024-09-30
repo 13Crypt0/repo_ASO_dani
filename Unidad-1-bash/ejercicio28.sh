7ed5d927e0bdfe05117fe7a34b496ec7885f7b07
Dani989898 <danmarcol2@alu.edu.gva.es>
2024-09-30 09:16:15 +0200

    Acabar ultimo

--- Unidad-1-bash/ejercicio28.sh
+++ Unidad-1-bash/ejercicio28.sh
@@ -1,17 +1,53 @@
-#!/bin/bash
+ç#!/bin/bash
 
 clear
 #Falta nombre de la red
 function informacion() {
-  ip=$(ip -o -f inet addr show enp0s3 | awk '{print $4}' | awk -F '/' '{print $1}')
-  CIDR=$(ip r | grep src | cut -d" " -f 1)
-  broadcast=$(ip -o -f inet addr show enp0s3 | awk '{print $6}')
-  marcara=$(ip -o -f inet addr show enp9s0 | awk '{print $4}' | awk -F '/' '{print $2}') #FALLO NO DEVUELVE MASCARA AL HABER DOS FILTRADOS
+
+  interfazred=$(ip route | grep "default via" | awk -F ' ' '{print $5}')
+  ip=$(ip -o -f inet addr show "$interfazred" | awk '{print $4}' | awk -F '/' '{print $1}')
+  CIDR=$(ip r | grep -E "$interfazred".*."src" | cut -d" " -f 1)
+  broadcast=$(ip -o -f inet addr show "$interfazred" | awk '{print $6}')
+  mascara=$("$CIDR" | cut -d/ -f2)
 
 echo "IP del equipo: $ip"
 echo "CIDR: $CIDR"
 echo "Broadcast: $broadcast"
-echo "Máscara de subred: /$mascara"
+echo "Máscara de subred: $mascara"
 }
 
 informacion
+
+function ping() {
+
+remake=$(echo "$ip" | awk -F '.' '{print $1 "." $2 "." $3 "." }')
+
+for (( i=1; i<=254; i++)); do
+  consultaip=$(ping -c 1 -w 1 "$remake$i" > /dev/null ; echo "$?")
+    if [[ "$consultaip" -ge 1 ]]; then
+      echo "La IP "$remake$i" ESTA LIBRE"
+    else
+      echo "La IP "$remake$i" NO ESTA LIBRE"
+    fi
+done
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function informe () {
+  echo "GENERANDO INFORME..."
+  echo ""
+  informacion >> Informe.txt
+  ping >> Informe.txt
+}
