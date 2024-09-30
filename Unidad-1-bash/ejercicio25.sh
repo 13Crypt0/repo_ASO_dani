b45ee659e58e870aa5936f09045bf50b00343a7c
Dani989898 <danmarcol2@alu.edu.gva.es>
2024-09-30 13:17:12 +0200

    Ejercicios Expresiones Regulares

--- Unidad-1-bash/ejercicio25.sh
+++ Unidad-1-bash/ejercicio25.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Realiza un script que muestre la lista de los últimos usuarios que iniciaron sesión, incluidas las direcciones IP Origen. (Solo debes mostrar las líneas en las que aparece una IP). Debes hacer uso del comando last y egrep.
 
 clear
 
--- Unidad-1-bash/ejercicio26.sh
+++ Unidad-1-bash/ejercicio26.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+#Realiza un script que busque cualquier fichero que pueda ser modificado por cualquier usuario (— — rwx) y guarde la lista de ficheros con la ruta exacta en el archivo archivos_peligrosos.txt
+
 clear
 
 echo "Generando lista archivos peligosos..."
 
+#Busqueda recursiva de los ficheros con permisos rwx en otros
+
 resultado=$(find / -exec ls -l {} \; 2> /dev/null | egrep '(^[-drwxst]{7}[rwx]{3})')
+
+#Agregando a archivos_peligrosos.txt solamente la ruta absoluta
 
 for i in $resultado; do
   echo "$i" | egrep "^.*/.*$" >> archivos_peligrosos.txt
