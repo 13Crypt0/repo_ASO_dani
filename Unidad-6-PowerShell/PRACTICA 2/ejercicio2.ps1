584d2fb1da7d6e5d3b3eea5ff9e28f60f9199138
Dani <danielmartinezcolomerr@gmail.com>
2024-12-18 09:43:08 +0100

    EJERCICIOS PRACTICA 2

---
+++ Unidad-6-PowerShell/PRACTICA 2/ejercicio2.ps1
@@ -0,0 +1,15 @@
+﻿[int] $numero1 = Read-Host "Introduce numero 1: "
+[int] $numero2 = Read-Host "Introduce numero 2: "
+
+#Suma
+$operacion_suma = $numero1 + $numero2
+Write-Host = "Resultado Suma: " $operacion_suma
+#Resta
+$operacion_resta = $numero1 - $numero2
+Write-Host = "Resultado Resta: " $operacion_resta
+#Multiplicacion
+$operacion_multiplicacion = $numero1 * $numero2
+Write-Host = "Resultado Multiplicacion: " $operacion_multiplicacion
+#Division
+$operacion_division = $numero1 / $numero2
+Write-Host = "Resultado Division: " $operacion_division
---
+++ Unidad-6-PowerShell/PRACTICA 2/ejercicio3.ps1
@@ -0,0 +1,4 @@
+﻿[int]$horas = Read-Host "Introduce las horas que tienes trabajadas: "
+[double]$valor_hora = Read-Host "Introduce cuanto vale la hora: "
+$salario = $horas * $valor_hora
+Write-Host = "Te pertenecen:"$salario
