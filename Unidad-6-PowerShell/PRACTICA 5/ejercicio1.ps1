26cc4533ea6b71a3c77e4d4c682ef48e5c073b98
Dani <danielmartinezcolomerr@gmail.com>
2025-01-13 12:15:22 +0100

    Ejercicios Practica 5 finalizados

---
+++ Unidad-6-PowerShell/PRACTICA 5/ejercicio1.ps1
@@ -0,0 +1,56 @@
+﻿[int]$numero1 = Read-Host "Introduce numero 1"
+[int]$numero2 = Read-Host "Introduce numero 2"
+
+Write-Host ""
+Write-Host "****** CALCULADORA ******"
+Write-Host ""
+Write-Host "1. Sumar"
+Write-Host "2. Restar"
+Write-Host "3. Multiplicar"
+Write-Host "4. Dividir"
+Write-Host "5. Salir"
+
+Function Sumar ($numero1, $numero2) {
+	$sumar = $numero1 + $numero2
+	Write-Host "El resultado de $numero1 + $numero2 es igual a $sumar"
+}
+
+Function Restar ($numero1, $numero2) {
+	$restar = $numero1 - $numero2
+	Write-Host "El resultado de $numero1 - $numero2 es igual a $restar"
+}
+
+Function Multiplicar ($numero1, $numero2) {
+	$multiplicar = $numero1 * $numero2
+	Write-Host "El resultado de $numero1 x $numero2 es igual a $multiplicar"
+}
+
+Function Dividir ($numero1, $numero2) {
+	$dividir = $numero1 / $numero2
+	Write-Host "El resultado de $numero1 / $numero2 es igual a $dividir"
+}
+
+[int]$opcion = Read-Host "Seleccione una opcion [1-5]"
+
+switch ($opcion) {
+    1 {
+        Sumar $numero1 $numero2
+    }
+    
+    2 {
+        Restar $numero1 $numero2
+    }
+
+    3 {
+        Multiplicar $numero1 $numero2
+    }
+
+    4 {
+        Dividir $numero1 $numero2
+    }
+
+    default {
+        Write-Host "Opcion no valida"
+    }
+
+}
---
+++ Unidad-6-PowerShell/PRACTICA 5/ejercicio2.ps1
@@ -0,0 +1,6 @@
+﻿$usuarios = Import-Csv "C:\Users\Administrador\Desktop\repo_ASO_dani\Unidad-6-PowerShell\PRACTICA 5\usuarios.csv"
+
+foreach ($user in $usuarios)
+{
+ 	Write-Host "Nombre: $($user.nombre) Apellidos: $($user.apellidos) Grupo: $($user.grupo)"
+}
