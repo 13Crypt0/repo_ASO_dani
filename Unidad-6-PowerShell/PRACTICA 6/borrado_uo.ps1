0755d489230fe6e61d9c2bb5c31bb1d4bf477439
Dani <danielmartinezcolomerr@gmail.com>
2025-01-15 09:58:40 +0100

    Terminar ejercicio alta

---
+++ Unidad-6-PowerShell/PRACTICA 6/borrado_uo.ps1
@@ -0,0 +1,6 @@
+﻿$nombre = Read-Host "Introduce nombre UO a eliminar"
+
+Set-ADOrganizationalUnit -Identity "OU=$nombre,DC=aso,DC=local" -ProtectedFromAccidentalDeletion $false
+Remove-ADOrganizationalUnit -Identity  "OU=$nombre,DC=aso,DC=local" -Recursive -Confirm:$False
+
+Write-Host "UO: $nombre - Eliminado"
---
+++ Unidad-6-PowerShell/PRACTICA 6/ejercicio1.ps1
@@ -0,0 +1,16 @@
+﻿New-ADOrganizationalUnit -Name "Empresa" -Path "DC=aso,DC=local" -Description "Unidad Empresa"
+
+$departamentos = Import-Csv C:\Users\Administrador\Desktop\archivos\departamentos.csv -Delimiter ";"
+foreach ($dept in $departamentos) {
+    New-ADOrganizationalUnit -Name "$($dept.departamento)" -Path "OU=Empresa,DC=aso,DC=local" -Description "$($dept.descripcion)"
+}
+
+foreach ($dept in $departamentos) {
+    New-ADGroup -Name "G-$($dept.departamento)" -GroupCategory Security -GroupScope Global -Path "OU=$($dept.departamento),OU=Empresa,DC=aso,DC=local"
+
+}
+
+#$empleados = Import-Csv C:\Users\Administrador\Desktop\archivos\empleados.csv -Delimiter ";"
+#foreach ($emp in $empleados) {
+#    New-ADUser -Name "$($emp.nombre) $($emp.apellido)" -Path "CN=Users,DC=aso,DC=local" -SamAccountName "$($emp.nombre)" -UserPrincipalName "mariag@empresa.local" -AccountPassword (ConvertTo-SecureString "aso2023." -AsPlainText -Force) -GivenName "Maria" -Surname "Garcia" -ChangePasswordAtLogon $true -Enabled $true
+#}
