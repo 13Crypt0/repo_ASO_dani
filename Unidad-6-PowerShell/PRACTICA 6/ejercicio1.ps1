b7f25c2f700aec63b446a0cb7df12a73c93cfc06
Dani <danielmartinezcolomerr@gmail.com>
2025-01-15 10:42:38 +0100

    Acabar ejercicio

--- Unidad-6-PowerShell/PRACTICA 6/ejercicio1.ps1
+++ Unidad-6-PowerShell/PRACTICA 6/ejercicio1.ps1
@@ -1,16 +1,25 @@
-﻿New-ADOrganizationalUnit -Name "Empresa" -Path "DC=aso,DC=local" -Description "Unidad Empresa"
+﻿# Creación de la Unidad Organzativa Empresa
+New-ADOrganizationalUnit -Name "Empresa" -Path "DC=aso,DC=local" -Description "Unidad Empresa"
 
+# Creación de Unidad Organizativa Departamentos
 $departamentos = Import-Csv C:\Users\Administrador\Desktop\archivos\departamentos.csv -Delimiter ";"
 foreach ($dept in $departamentos) {
     New-ADOrganizationalUnit -Name "$($dept.departamento)" -Path "OU=Empresa,DC=aso,DC=local" -Description "$($dept.descripcion)"
 }
 
+# Creación Grupos Departamentos
 foreach ($dept in $departamentos) {
     New-ADGroup -Name "G-$($dept.departamento)" -GroupCategory Security -GroupScope Global -Path "OU=$($dept.departamento),OU=Empresa,DC=aso,DC=local"
 
 }
 
-#$empleados = Import-Csv C:\Users\Administrador\Desktop\archivos\empleados.csv -Delimiter ";"
+# Creación Empleados
+$empleados = Import-Csv C:\Users\Administrador\Desktop\archivos\empleados.csv -Delimiter ";"
+foreach ($emp in $empleados) {
+    New-ADUser -Name "$($emp.nombre) $($emp.apellido)" -Path "OU=$($emp.departamento),OU=Empresa,DC=aso,DC=local" -SamAccountName "$($emp.nombre).$($emp.apellido)" -UserPrincipalName "$($emp.nombre)@aso.local" -AccountPassword (ConvertTo-SecureString "aso2025." -AsPlainText -Force) -GivenName "$($emp.nombre)" -Surname "$($emp.apellido)" -ChangePasswordAtLogon $true -Enabled $true
+}
+
+
 #foreach ($emp in $empleados) {
-#    New-ADUser -Name "$($emp.nombre) $($emp.apellido)" -Path "CN=Users,DC=aso,DC=local" -SamAccountName "$($emp.nombre)" -UserPrincipalName "mariag@empresa.local" -AccountPassword (ConvertTo-SecureString "aso2023." -AsPlainText -Force) -GivenName "Maria" -Surname "Garcia" -ChangePasswordAtLogon $true -Enabled $true
+#    Add-ADGroupMember -Identity "$($emp.departamento)" -Members $($emp.nombre).$($emp.apellido)
 #}
