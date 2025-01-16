d6114af407a6dfc17fb00b16c314cb18697168b0
Dani <danielmartinezcolomerr@gmail.com>
2025-01-16 11:45:00 +0100

    Ejercicio Active Directory con PowerShell Finalizado

--- Unidad-6-PowerShell/PRACTICA 6/ejercicio1.ps1
+++ Unidad-6-PowerShell/PRACTICA 6/ejercicio1.ps1
@@ -1,25 +1,24 @@
-﻿# Creación de la Unidad Organzativa Empresa
+﻿Write-Host "Creando Unidad Organizativa Empresa..."
 New-ADOrganizationalUnit -Name "Empresa" -Path "DC=aso,DC=local" -Description "Unidad Empresa"
 
-# Creación de Unidad Organizativa Departamentos
+Write-Host "Craendo Unidades Organizativas Departamentos..."
 $departamentos = Import-Csv C:\Users\Administrador\Desktop\archivos\departamentos.csv -Delimiter ";"
 foreach ($dept in $departamentos) {
     New-ADOrganizationalUnit -Name "$($dept.departamento)" -Path "OU=Empresa,DC=aso,DC=local" -Description "$($dept.descripcion)"
 }
 
-# Creación Grupos Departamentos
+Write-Host "Creando Grupos Departamento..."
 foreach ($dept in $departamentos) {
     New-ADGroup -Name "G-$($dept.departamento)" -GroupCategory Security -GroupScope Global -Path "OU=$($dept.departamento),OU=Empresa,DC=aso,DC=local"
-
 }
 
-# Creación Empleados
+Write-Host "Creando usuarios..."
 $empleados = Import-Csv C:\Users\Administrador\Desktop\archivos\empleados.csv -Delimiter ";"
 foreach ($emp in $empleados) {
     New-ADUser -Name "$($emp.nombre) $($emp.apellido)" -Path "OU=$($emp.departamento),OU=Empresa,DC=aso,DC=local" -SamAccountName "$($emp.nombre).$($emp.apellido)" -UserPrincipalName "$($emp.nombre)@aso.local" -AccountPassword (ConvertTo-SecureString "aso2025." -AsPlainText -Force) -GivenName "$($emp.nombre)" -Surname "$($emp.apellido)" -ChangePasswordAtLogon $true -Enabled $true
 }
 
-
-#foreach ($emp in $empleados) {
-#    Add-ADGroupMember -Identity "$($emp.departamento)" -Members $($emp.nombre).$($emp.apellido)
-#}
+Write-Host "Agregando usuarios a grupos..."
+foreach ($emp in $empleados) {
+    Add-ADGroupMember -Identity "G-$($emp.departamento)" -Members "$($emp.nombre).$($emp.apellido)"
+}
