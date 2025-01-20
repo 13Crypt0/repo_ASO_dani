ca18e31901ca1d9c268d30f18e837904af8e6fc7
Dani <danielmartinezcolomerr@gmail.com>
2025-01-20 13:52:27 +0100

    Acabar

---
+++ Unidad-6-PowerShell/PRACTICA 7/ejercicio1.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "Imprimiendo carpetas compartidas..."
+Get-SmbShare | Where-Object { $_.Name -notin 'NETLOGON', 'SYSVOL'} | Where-Object {$_.Name -notlike '*$' }
+
+Write-Host "Creando directorio ASO"
+New-Item C:\ASO -ItemType directory
+
+Write-Host "Compartiendo ASO..."
+New-SmbShare -Path C:\ASO -Name ASO
+
+Grant-SmbShareAccess -Name ASO -AccountName profesores -AccessRight Full -Force
+#Grant-SmbShareAccess -Name ASO -AccountName profesores -AccessRight Full -Force
